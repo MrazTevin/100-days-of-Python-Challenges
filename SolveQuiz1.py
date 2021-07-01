2610125c5428488bcd0593bf9dbdcf9c21af575b
Mraz Tevin <millatevin@gmail.com>
2021-07-01 11:57:17 +0300

    completely solved challenge

--- SolveQuiz1.py
+++ SolveQuiz1.py
@@ -1,21 +1,14 @@
 # function to determine leap year in the gregorian calendar
 # if a year is leap year, return Boolean true, otherwise return false
-# if year is evenly divided by 4, then its a leap year
-# if year is not evenly divided by 100 its not a leap year
-# if year is evenly divided by 400 then its a leap year
+# if the year can be evenly divided by 4, it's a leap year, unless: The year can be evenly divided by 100 it is# not a leap year,unless the year is also divisible by 400, then its a leap year
 # In the Gregorian Calendar, years 2000 and 2400 are leap years, while 1800,1900,2100,2200,2300 and 2500 are not leap years
 
-def leap_year():
-    year = int(input("Input any year: "))
-    if (year % 4 == 0):
-      if (year % 100 == 0):
-        if (year % 400 == 0):
-            print("It's a leap year")
-        else:
-            print("It's not a leap year")
-      else:
-        print("It's a leap year")
+def leap_year(year):
+    if (year%4==0 and year%100!=0):
+        return True
+    elif (year%100==0 and year%400==0):
+        return True
     else:
-        print("It's not a leap year")
-
-leap_year()
+        return False
+yearInput = int(input("Enter any year i.e 2001 :"))
+print(leap_year(yearInput))
